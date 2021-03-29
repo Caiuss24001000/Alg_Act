cf0a1975db5834c9c58fcd11a4df17f60f34fcd8
Sergio Rodriguez <ingensiscomescom@outlook.com>
2021-03-29 00:32:18 -0600

    Se agregaron las acuaciones de complejidad hasta el codigo 5 y un pequeño analisis del codigo 6 Se agregaron las graficas en formato .tex hasta el codigo 5

--- Ejercicios 2/ejercicio2.c
+++ Ejercicios 2/ejercicio2.c
@@ -32,8 +32,21 @@ void c7(int [], int);
 int main(){
     int n[]={-1,0,1,2,3,5,15,20,100,409,500,593,1000,1471,1500,2801,3000,5000,10000,20000};
     int ct, ce;
-    
-    c2(n,TAM);
+    int A[TAM][TAM], B[TAM][TAM], C[TAM][TAM];
+    for (int i = 0; i < TAM; ++i)
+    {
+        for (int j = 0; j < TAM; ++j)
+        {
+            A[i][j]=n[j];
+            B[i][j]=n[j];
+            C[i][j]=n[j];
+        }
+    }
+    c1(n, TAM);
+    c2(n, TAM);
+    c3(A, B, C, TAM);
+    c4(TAM);
+    c5(n, n, TAM);
     return 0;
 }
 
@@ -51,12 +64,11 @@ void c1(int A[], int n){
             A[j] = A[j+1]; t++; // (log2(n))
             A[j+1] = temp; t++; // (log2(n))
             t++; //j/2 log2(n)
-            printf("Hola %d %d\n",j,j);
         }
-        //printf("Hola %d\n", i);
         t++;//n-1
     }
     t++;
+
     printf("La variable temporal es: %d\n", t);
 }
 
@@ -91,59 +103,84 @@ void c3(int A[TAM][TAM], int B[TAM][TAM], int C[TAM][TAM], int n){
         for (int j = 1; j < n; ++j){ //Todo por n-1
             t++; //Comparaciones lo hace n
             C[i][j] = 0; t++; // lo hace n-1
-
             t++; // lo hace una vez por n-1
-            for (int k=1; i < n; ++i){ // Todo por n-1
+            for (int k=1; k < n; ++k){ // Todo por n-1
                 t++; //Comparaciones lo hace n
                 C[i][j] = C[i][j] + A[i][k]*B[k][j]; t+=3; // lo hace 3 (n-1)
                 t++; //el incremento lo hace n-1
             }
-            t++; // lo hace n-1 incrementos
+            t++; // comparaciones de k<n final (no contabilizar)
+            t++; // lo hace n-1 incrementos de j++ 
         }
-        t++;
-        t++; // j++ lo hace (n-1)
+        t++; // comparaciones de j<n final (no contabilizar)
+        t++; // i++ lo hace (n-1)
     }
+    t++; // comparacion de i<n final (no contabilizar)
 
-    t++; //Complemento de la conparacion 
+    printf("La variable temporal es: %d\n", t);
 }
 
 void c4(int n){
     int aux;
-
+    int t = 0;
     //Inicio
-    int anterior = 1;
-    int actual = 1;
-    while(n>2){
-        aux = anterior + actual;
-        anterior = actual;
-        actual = aux;
-
-        n = n - 1;
+    int anterior = 1; t++; // se hace 1
+    int actual = 1; t++; // se hace 1
+    while(n>2){ 
+        t++; // se hace n-1 comprobaciones
+        aux = anterior + actual; t+=2; // se hace 2(n-2)
+        anterior = actual; t++; // se hace n-2
+        actual = aux; t++; // se hace n-2
+        n = n - 1; t++; // se hace n-2 decrementos
     }
+    t++; // comparacion de n>2 final (no contabilizar)
+
+    printf("La variable temporal es: %d\n", t);
 }
 
-void c5(int s[], int s2[], int n){
-    for (int i = n-1,j=0; i >= 0; i--, j++){
-        s2[j] = s[i];
+void c5(int n1[], int n2[], int n){
+    int *s = n1, *s2 = n2;
+    int t = 0;
+
+    //inicio
+    t+=2; // se hace i=n-1 y j=0, se contabiliza con 2
+    for (int i = n-1,j=0; i >= 0; i--, j++){ 
+        t++; // se hace n+1 comprobaciones 
+        s2[j] = s[i]; t++;// se hace n 
+        t+=2; // se hace 2(n) incrementos o decrementos de i y j
     }
-    for (int k=0; k<n; ++k)
-    {
+    t++; // comparacion de i>=0 final (no contabilizar)
+    t++; // se hace 1
+    for (int k=0; k<n; ++k){
+        t++; // se hace n+1 comprobaciones
         //Aqui no esta declarada la "i" para esta instruccion s[i] = s2[i]; por lo que tomaremos a k como si fuera i
-        s[k] = s2[k]; 
+        s[k] = s2[k]; t++; // se hace n
+        t++; // se hace n incrementos de k
     }
+    t++; // comparacion de k<n final (no contabilizar)
+    
+    printf("La variable temporal es: %d\n", t);
 }
 
 void c6(int a, int b){
-    int l,r;
+    int l,r, t = 0;
 
     //Inicio
-    l=(a<b)?a:b;
-    r=1;
-    for (int i=2; i<=1; ++i)
-    {
-        if(a%i==0 && b%i==0)
-            r=i;
+    l=(a<b)?a:b; t+=2; // se hace una asignacion y una condicional se contabiliza con 2
+    r=1; t++; // se hace 1
+    t++; // se hace 1
+    for (int i=2; i<=l; ++i){ // siempre se compara con el mas pequeño entre a y b, tomaremos n como ese valor
+        t++; // se hace n comprobaciones
+        t+=3;
+        if(a%i==0 && b%i==0){ // se hace 3(n-1)
+            r=i; t++; // se hace ->
+            printf("La variable es %d , %d, %d\n", a, b, i);
+        }
+        t++; // se hace n-1 incrementos de i
     }
+    t++; // comparacion de i<=l final (no contabilizar)
+
+    printf("La variable temporal es: %d\n", t);
 }
 
 void c7(int lista[], int n){
@@ -163,25 +200,3 @@ void c7(int lista[], int n){
     }
 }
 
-/*
-
-for i = 0 to n do
-        for j = 1 to n do
-            C[i,j] = 0;
-            for k=1 to n do
-                C[i,j] = C[i,j] + A[i,k]*B[k,j];
-
-void c3(int A[][], int B[][], int C[][], int n){
-
-    for (int i = 1; i < n; ++i)
-    {
-        for (int j = 1; j < n; ++j){
-            C[i,j] = 0;
-            for (int k=1; i < n; ++i){
-                C[i][j] = C[i][j] + A[i][k]*B[k][j];
-            }
-        }
-    }
-}
-
-*/
--- Grafica/grafica.c
+++ Grafica/grafica.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define TAM_MAX 500000
 
-//#include <math.h>
-//#define LOG2 log2(x) = log10(x) / log10(2)
-
-double log_2(int, double);
-double p(double, int);
+double log_2(int, double);      //Logaritmo de una base dada
+double p(double, int);          //Potencia
 double fc1(int);
 double fc2(int);
+double fc3(int);
+double fc4(int);
+double fc5(int);
 
 int main(){
     
@@ -20,13 +21,39 @@ int main(){
 
     for (int i = 0; i <= TAM_MAX; i+=500)
     {
-        double a = fc2(i);
+        double a = fc5(i);
         fprintf(pf, "%d, %f \n", i, a);
         //printf("%d %f\n", i, a);
     }
+
+    /*
+    int n[20];
+    for (int i = 0; i < 20; ++i)
+    {
+        n[i]=i*1.7;
+    }
+    
+    for (int i = 0; i < 20; ++i)
+    {
+        int a = (n[i]*10)%(i+1);
+        printf("n: %d modulo i: %d es: %d\n", n[i]*10, i+1, a);
+    }
+    */
     
     fclose(pf);
     return 0;
+}
+
+double fc5(int n){
+    return 7*n+5;
+}
+
+double fc4(int n){
+    return 6*n-7;
+}
+
+double fc3(int n){
+    return 5*p(n, 3) - 10*p(n,2) + 9*n - 2;
 }
 
 double fc2(int i){
@@ -37,6 +64,7 @@ double fc1(int i){
     return 4*i+2+5*i*log_2(2,(double)i)+5*log_2(2,(double)i);
 }
 
+//Funcion para hacer logaritmo de un numero(n) en una base(b) dada
 double log_2(int b,double n) {
     double val = 0;
     int i,accurate = 10,reps=0;
@@ -49,9 +77,11 @@ double log_2(int b,double n) {
     return (double)val/p(10,reps);
 }
 
-double p(double x,int i) {
+//Funcion para elevar a un exponenete entero dado, retorna el resultado
+double p(double numero,int exponente) {
     double r = 1.0;
-    for(i;i>0;i--) r *= x;
+    for(exponente;exponente>0;exponente--) 
+        r *= numero;
     return r;
 }
 
